3cfdc8062c43d0f8cd8445f6db986c661d8ddaf2
Alexander Moreira de Morais <alex.moreira@gmail.com>
2023-02-28 23:13:44 -0300

    Comandos aninhados em uma String

---
+++ Strings.py
@@ -0,0 +1,7 @@
+# Utilizando diversos comandos aninhados
+titulo = ("[Alexander Moreira de Morais]" 
+            .replace("[", "")   
+            .replace("]", "")   
+            .title()            
+            .center(40, "*"))
+print(titulo)
