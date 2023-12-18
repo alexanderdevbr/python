984ab9d9595bc2940fdd2a780a78101d0c57ef40
Alexander Moreira de Morais (TR301005) <alexander.morais@trf1.jus.br>
2023-12-18 16:05:23 -0300

    Criacao de algoritmo para calcular Desvio Padrao

---
+++ Estatistica/DesvioPadrao.py
@@ -0,0 +1,33 @@
+def desvio(serie):
+    media = sum(serie) / len(serie)
+
+    # Gerando vetor com o quadrado da diferenca entre cada um dos valores da serie e a media
+    quadrados_diferenca_media = []
+    for i in serie:
+        v = i - media
+        quadrados_diferenca_media.append(v * v)
+    variancia = (sum(quadrados_diferenca_media) / len(quadrados_diferenca_media))
+
+    # Calculando a Variancia que e a media do vetor com o quadrado da diferenca
+    desvio = variancia ** 0.5
+
+    return media, quadrados_diferenca_media, variancia, desvio
+
+
+
+# Algoritmo para calcular desvio padrao de uma serie de numeros
+serie = [6,2,3,1]
+m, qdm, v, d = desvio(serie)
+print("Serie.........:", serie)
+print("Quadrado Dif..:", qdm)
+print("Media.........: %.2f" % m)
+print("Variancia.....: %.2f" % v)
+print("Desviro Padrao: %.2f" % d)
+print("")
+serie = [1,4,7,2,6]
+m, qdm, v, d = desvio(serie)
+print("Serie.........:", serie)
+print("Quadrado Dif..:", qdm)
+print("Media.........: %.2f" % m)
+print("Variancia.....: %.2f" % v)
+print("Desviro Padrao: %.2f" % d)
